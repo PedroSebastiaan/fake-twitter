95070766b4bd60083a1a7a8cc775d5017d24830d
PedroSebastiaan <pizarropedrov@gmail.com>
2021-08-18 12:46:32 -0400

    fix: change anchor to lito

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @tweets = Tweet.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions', 
+    registrations: 'users/registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
