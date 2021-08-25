b09dde96b1f7b180ce2d739696d6488fc018ea4f
PedroSebastiaan <pizarropedrov@gmail.com>
2021-08-25 00:23:33 -0400

    fix: follows controller

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,24 +1,24 @@
-class LikesController < ApplicationController
+class FollowsController < ApplicationController
     before_action :set_user, only: %i[ destroy ]
 
     def create
-    @user = current_user
-    @tweet = Tweet.find(params[:id])
-    @added = @tweet.user_id
-    @follow = Follow.new(follow_params.merge(user_id: @user.id, follow_id: @added))
-    respond_to do |format|
-        if @follow.save
-        format.html { redirect_to root_path }
-        else
-        format.html { render :new, status: :unprocessable_entity }
+        @tweet = Tweet.find(params[:id])
+        @added = @tweet.user_id
+        @user = current_user
+        @follow = Follow.new(follow_params.merge(user_id: @user.id, follow_id: @added))
+        respond_to do |format|
+            if @follow.save
+            format.html { redirect_to root_path }
+            else
+            format.html { render :new, status: :unprocessable_entity }
+            end
         end
-    end
     end
 
     def destroy
-    @follow = Follow.find(params[:id])
-    @follow.destroy
-    redirect_to root_path 
+        @follow = Follow.find(params[:id])
+        @follow.destroy
+        redirect_to root_path 
     end
 
     private
@@ -28,14 +28,6 @@ class LikesController < ApplicationController
 
     def follow_params
         params.fetch(:follow, {}).permit(:user_id, :follow_id)
-    end
-
-    def set_tweet
-        @tweet = Tweet.find(params[:id])
-    end
-
-    def tweet_params
-        params.require(:tweet).permit(:content, :retweet_id, :date)
     end
 
     def set_user
