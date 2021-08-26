e2bd981522e3e74c193258a3084bed24ce330dc5
PedroSebastiaan <pizarropedrov@gmail.com>
2021-08-26 17:05:12 -0400

    add: tweet model to activeadmin

---
+++ app/admin/tweets.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Tweet do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :content, :retweet_id, :date, :user_id, :retweet_count
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:content, :retweet_id, :date, :user_id, :retweet_count]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
