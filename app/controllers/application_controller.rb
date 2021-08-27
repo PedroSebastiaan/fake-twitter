c850892526dec761e5933f19dac6c01259b8f3b1
PedroSebastiaan <pizarropedrov@gmail.com>
2021-08-27 14:55:44 -0400

    add: ban function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :check_status
+
+    def check_status
+        if current_user && current_user.suspended == true
+            sign_out current_user 
+            redirect_to new_user_session_path, notice: 'You were banned!'
+        end
+    end
 end
