05c01374b75a2cf9c92f428ff7678d3834e01a86
PedroSebastiaan <pizarropedrov@gmail.com>
2021-08-26 21:58:19 -0400

    fix: admin users login

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -10,7 +10,7 @@ ActiveAdmin.register User do
       Tweet.where(user_id: u.id, retweet_id: nil).count
     end
     column "retweets" do |u|
-      (Tweet.where(user_id: u.id).count) - (Tweet.where(user_id: u.id, retweet_id: nil).count)
+      (Tweet.where(user_id: u.id)).where.not(retweet_id: nil).count
     end
     column "likes" do |u|
       Like.where(user_id: u.id).count
