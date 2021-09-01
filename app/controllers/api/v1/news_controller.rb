5e38eaeb831e19b6924c1cb9867bfa401cd7811a
PedroSebastiaan <pizarropedrov@gmail.com>
2021-08-31 21:52:38 -0400

    fix: last_tweets api body

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -4,12 +4,14 @@ module Api
             def last_tweets
                 @tweets = Tweet.last(50)
                 last_tweets = @tweets.map do |tweet|
-                    id =  tweet.id,
-                    content = tweet.content,
-                    user_id = tweet.user_id,
-                    like_count = Like.where(tweet_id: tweet.id).count,
-                    retweet_count = Tweet.where(retweet_id: tweet.id).count,
-                    retweeted_from = tweet.retweet_id
+                    {
+                        "id":  tweet.id,
+                        "content": tweet.content,
+                        "user_id": tweet.user_id,
+                        "like_count": Like.where(tweet_id: tweet.id).count,
+                        "retweet_count": Tweet.where(retweet_id: tweet.id).count,
+                        "retweeted_from": tweet.retweet_id
+                    }
                 end
                 render json: last_tweets
             end
